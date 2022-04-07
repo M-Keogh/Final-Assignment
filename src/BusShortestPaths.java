0d27985ce2709d2e4e506801f17e781e51c22f99
Michael Keogh <keoghm5@tcd.ie>
2022-04-07 15:17:10 +0100

    Initial Commit

--- src/BusShortestPaths.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class BusShortestPaths {
-    //TODO Implement Assignment Code
-    public static void main(String[] args) {
-        int day = 0;
-        System.out.println("Day: " + day);
-    }
-}
---
+++ src/ShortestPath.java
@@ -0,0 +1,5 @@
+public class ShortestPath {
+    void print(){
+        System.out.println("Shortest path is 3");
+    }
+}
